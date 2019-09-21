956aa34a9fbaf0ed3db1bc54e496bfd9c747b049
Jay Morgan <jaymiles17@gmail.com>
2019-09-21 16:41:56 +0300

    added argparse for convience

--- DPnorm.jl
+++ DPnorm.jl
@@ -11,23 +11,33 @@ using DataFrames
 import StatsBase: countmap
 using ProgressMeter
 using LightXML
+using ArgParse
 
 # Internal imports
 include("includes/stats.jl");
 include("includes/data.jl");
 
-# setup the datapaths and count the number of corpus parts
-const DATA_DIR = "data"
-const CONV_DIR = joinpath(DATA_DIR, "test")
+
+# command line arguments
+s = ArgParseSettings()
+@add_arg_table s begin
+    "--input", "-i"
+        help = "input directory of corpus parts"
+    "--output", "-o"
+        help = "filename of the output CSV filename"
+end
+const ARGS = parse_args(s)
+
+# setup the datapaths and count the number of corpus part
+const CONV_DIR = ARGS["input"]
+const OUTPUT_FN = ARGS["output"]
 corpus_parts = read_dir(CONV_DIR)
-
-
-
 
 # http://www.stgries.info/research/ToApp_STG_Dispersion_PHCL.pdf
 n = length(corpus_parts)    # the length of the corpus in parts
 s = [];
 
+@info "Reading Corpus Parts from $CONV_DIR"
 # get all the words per corpus
 words_in_parts = [
     read_corpus_part(joinpath(CONV_DIR, corpus_part)) |> find_words
@@ -40,21 +50,18 @@ v = [countmap(part) for part in words_in_parts] # the frequences of a in each pa
 f = countmap(split(join([join(w, " ") for w in words_in_parts], " "), " "))  # the overall frequencies
 
 norms = Dict{String,Float64}()
-for (word, total_freq) in f
+@showprogress 1 "Computing DPNorm Scores" for (word, total_freq) in f
     dp = 0
     for i in 1:n
         of = get(v[i], word, 0)
         dp += abs((of / total_freq) - s[i])
     end
-
-    dp *= 0.5
-
-    norms[word] = dp / (1 - minimum(s))
+    norms[word] = (0.5*dp) / (1 - minimum(s))
 end
 
 norms = sort(collect(norms), by=x->x[2])
-@info "Writing DPNorm scores to"
+@info "Writing DPNorm score to $OUTPUT_FN"
 norms = DataFrame(token = [x[1] for x in norms],
                    frequency = [f[w[1]] for w in norms],
                    dpnorm = [x[2] for x in norms]);
-CSV.write("output.csv", norms)
+CSV.write(OUTPUT_FN, norms)
